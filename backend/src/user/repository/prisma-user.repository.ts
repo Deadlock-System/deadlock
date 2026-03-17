12144991a4e61f0e303510a1461784fac7d031a4
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2026-03-17 19:08:49 -0300

    feat(backend): ✨ Add repository implementation with prisma for user domain

---
+++ backend/src/user/repository/prisma-user.repository.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '../entities/user.entity';
+import { UserRepository } from './user.repository';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UserMapper } from '../mappers/user.mapper';
+
+@Injectable()
+export class PrismaRepository implements UserRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async create(user: User): Promise<User> {
+    const newUser = await this.prisma.user.create({
+      data: UserMapper.toPrisma(user),
+    });
+    return UserMapper.toDomain(newUser);
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return null;
+    }
+
+    return UserMapper.toDomain(user);
+  }
+
+  async findByUsername(username: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        user_name: username,
+      },
+    });
+
+    if (!user) {
+      return null;
+    }
+
+    return UserMapper.toDomain(user);
+  }
+}
