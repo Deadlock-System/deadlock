c8f111f7c4c3bacdb822a25a7f54685c4efc3c6d
Dannylo Torres <krdannylo.new@gmail.com>
2026-03-21 01:08:02 -0300

    security(backend): 🔒 Add forms to protect api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,12 +5,23 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { AppException } from './common/exceptions/app.exception';
 import { RequestErrorCode } from './common/exceptions/error-codes/request-error.code';
 import { RequestErrorMessages } from './common/exceptions/error-messages/request-error-messages';
+import { SanitizePipe } from './common/pipes/sanitize.pipe';
+import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+
+  app.enableCors({
+    origin: '*',
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
       exceptionFactory: () => {
         return new AppException({
           code: RequestErrorCode.INVALID_REQUEST_FORMAT,
@@ -19,9 +30,19 @@ async function bootstrap() {
         });
       },
     }),
+    new SanitizePipe()
   );
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Deadlock API')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('docs', app, documentFactory);
 
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
