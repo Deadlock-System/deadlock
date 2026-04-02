9241c4e2e37fa6009f11f9240c7d5e76b20df3e1
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2026-04-02 19:08:14 -0300

    feat(backend): Add all the swagger mapping of `PostsController` endpoints

--- backend/src/modules/content/posts/dto/create-post.dto.ts
+++ backend/src/modules/content/posts/dto/create-post.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   ArrayMaxSize,
   IsArray,
@@ -6,19 +7,24 @@ import {
   IsString,
   MaxLength,
 } from 'class-validator';
+import { CreatePostDocs } from './posts.swagger';
 
 export class CreatePostDto {
+  @ApiProperty(CreatePostDocs.title)
   @IsString()
   @MaxLength(255)
   title: string;
 
+  @ApiProperty(CreatePostDocs.content)
   @IsString()
   @MaxLength(20000)
   content: string;
 
+  @ApiProperty(CreatePostDocs.anonymous)
   @IsBoolean()
   anonymous: boolean;
 
+  @ApiProperty(CreatePostDocs.languages)
   @IsArray()
   @IsString({ each: true })
   @ArrayMaxSize(5, { message: 'Você só pode adicionar até 5 linguagens.' })
