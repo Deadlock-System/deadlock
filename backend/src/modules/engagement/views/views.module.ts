a0f86b1f4d579dd1826a81a03f9c40a247682a71
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2026-04-05 22:41:11 -0300

    feat(backend): Add sync view cron job logic

--- backend/src/modules/engagement/views/views.module.ts
+++ backend/src/modules/engagement/views/views.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ViewsService } from './views.service';
+import { ViewsCron } from './views.cron';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  providers: [ViewsService],
+  imports: [PrismaModule],
+  providers: [ViewsService, ViewsCron],
   exports: [ViewsService],
 })
 export class ViewsModule {}
